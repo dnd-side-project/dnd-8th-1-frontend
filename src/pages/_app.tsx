fbc835d29c95054d02a886021e4aea4e5ab6764f
jinyoung <sm099@naver.com>
2023-01-16 21:56:11 +0900

    :wrench: env : 일부 라이브러리 설치
    1. yup, react-hook-form, react-query-devtools 설치 2. react-query-devtools 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
