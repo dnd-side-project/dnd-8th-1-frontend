c0799e3d42db3fde185824ccb66ca4c2bc1744a9
jinyoung <87177577+jinyoung234@users.noreply.github.com>
2023-01-24 15:52:55 +0900

    🔧 env: React Query & React Query Devtools v4로 마이그레이션 (#6)
    🔧 env : React Query & React Query Devtools v3에서 v4로 마이그레이션
    
    🔧 env : _app.tsx 내 v3 세팅 v4로 마이그레이션

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
