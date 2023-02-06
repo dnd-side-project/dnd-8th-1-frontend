8caddf2db43662adcdc20a80ca42eafaa0db00ca
jinyoung <sm099@naver.com>
2023-02-06 16:13:24 +0900

    ✨ feat : 라벨 컴포넌트 스토리북 구현
    라벨 컴포넌트 스토리북 구현 완료

---
+++ src/components/atoms/Label/Label.stories.tsx
@@ -0,0 +1,19 @@
+import Label from '.'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+export default {
+  component: Label,
+  title: 'Atoms/Label',
+  parameters: {
+    componentSubtitle: '공통 컴포넌트/라벨',
+  },
+} as ComponentMeta<typeof Label>
+
+const Template: ComponentStory<typeof Label> = (args) => <Label {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  content: '라벨 입니다.',
+  className: '',
+  htmlFor: 'label',
+}
