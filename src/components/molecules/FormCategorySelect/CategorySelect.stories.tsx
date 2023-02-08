89c83de50ba993d8a27119e00e1aa6b9e144bb01
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-08 18:47:27 +0900

    💡 comment: 필요없는 주석 제거

--- src/components/molecules/FormCategorySelect/CategorySelect.stories.tsx
+++ src/components/molecules/FormCategorySelect/CategorySelect.stories.tsx
@@ -2,19 +2,16 @@ import FormCategorySelect from '.'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { Box } from '@chakra-ui/react'
 
-/**
- * 스토리북 확인을 위한 샘플 파일입니다.
- */
 export default {
   component: FormCategorySelect,
   title: 'MoleCules/FormCategorySelect',
   parameters: {
-    componentSubtitle: '공통 컴포넌트로 사용되는 컴포넌트',
+    componentSubtitle: '작성 폼 페이지에서 카테고리를 설정하는 컴포넌트',
   },
 } as ComponentMeta<typeof FormCategorySelect>
 
 const callback = (index: number) => {
-  console.log('상위 컴포넌트에서 콜백 넘겨주기', index)
+  console.log('상위 컴포넌트에서 콜백 넘겨주기, 선택된 인덱스 : ', index)
 }
 
 const Template: ComponentStory<typeof FormCategorySelect> = (args) => {
