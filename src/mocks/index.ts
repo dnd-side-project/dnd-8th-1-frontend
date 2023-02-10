46be17c59a80aefb3110002122a36a558705a2f4
jinyoung <sm099@naver.com>
2023-02-10 16:52:11 +0900

    🤡 mock : msw 초기 세팅 진행
    msw 초기 세팅 진행 완료

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,1 +1,15 @@
-export {}
+export * from './server'
+export * from './browser'
+export * from './handlers'
+
+export async function initMocks() {
+  if (typeof window === 'undefined') {
+    const { server } = await import('@mocks')
+    server.listen()
+  } else {
+    const { worker } = await import('@mocks')
+    worker.start()
+  }
+}
+
+initMocks()
