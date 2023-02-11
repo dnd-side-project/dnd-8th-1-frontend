901d35ccfa986e33ca7f89b518899057b2a159f1
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-12 03:21:21 +0900

    💄 design: 스타일 관련 상수 정의

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,43 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      green: {
+        light: '#69FFA5',
+        DEFAULT: '#18FF74',
+      },
+      blue: {
+        light: '#55ADFF',
+        DEFAULT: '#1890FF',
+      },
+      violet: {
+        light: '#6236FD',
+        DEFAULT: '#9577FF',
+      },
+      gray: {
+        900: '#080808',
+        700: '#2D2D2D',
+        600: '#515151',
+        500: '#747474',
+        400: '#A3A3A3',
+        300: '#C9C9C9',
+        200: '#FEFEFE',
+      },
+    },
+    backgroundImage: {
+      green_gradient: 'linear-gradient(64.24deg, #18FF74 12.45%, #18ACFF 100%)',
+      blue_gradient:
+        'linear-gradient(61.53deg, #2400A2 -5.41%, #374BFF 35.21%, #18FF74 100%)',
+      violent_gradient:
+        'linear-gradient(61.53deg, #673CFF 30.27%, #1D77FF 100%)',
+    },
+    boxShadow: {
+      shadow: '0px 1px 0px rgba(255, 255, 255, 0.1)',
+      blur: '0px 0px 20px rgba(164, 170, 172, 0.25)',
+    },
+    screens: {
+      mobile: '325px',
+    },
   },
   plugins: [],
 }
