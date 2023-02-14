523b64e6edcbc5153c1d9cdca19162802fcffdc7
jinyoung <sm099@naver.com>
2023-02-14 22:14:33 +0900

    ✨ feat : 완료된 API 명세에 대한 인터페이스 정의
    완료된 API 명세에 대한 인터페이스 정의 완료

---
+++ src/types/apis/meet/index.ts
@@ -0,0 +1,32 @@
+import {
+  BaseResponse,
+  Meet,
+  MeetAccept,
+  MeetCandicate,
+  MeetDetail,
+  PaginationResponse,
+} from '@types'
+
+// 만나기 전체 조회 응답
+export interface MeetResponse extends BaseResponse {
+  data?: PaginationResponse<Meet>
+}
+
+// 만나기 상세 조회 응답
+export interface MeetDetailResponse extends BaseResponse {
+  data?: MeetDetail
+}
+
+// 만나기 글 등록 / 수정 요청
+export type MeetEditRequest = Omit<MeetDetail, 'profile'>
+
+// 만나기 글 삭제 요청
+export type MeetDeleteRequest = Pick<MeetDetail, 'eventId'>
+
+// 만나기 신청자 조회 응답
+export interface MeetCandicateResponse extends BaseResponse {
+  data?: MeetCandicate[]
+}
+
+// 만나기 매칭 하기 요청
+export type MeetMatchRequest = MeetAccept
---
+++ src/types/apis/model.ts
@@ -0,0 +1,107 @@
+import { GenreTypes, MeetTypes } from '@types'
+
+export interface User {
+  memberId?: string
+  memberName?: string
+  memberEmail?: string
+  isSignUp?: string
+  role?: string
+  profileId?: string
+  profileType?: string
+  profileName?: string
+  profileImg?: string
+  profileGenre?: string
+  location?: string
+  careerStartDay?: string
+  description?: string
+  openChatUrl?: string
+  portfolioUrl?: {
+    youtubeUrl?: string
+    instagramUrl?: string
+    twitterUrl?: string
+  }
+}
+
+export interface Meet
+  extends Pick<User, 'profileId' | 'profileName' | 'profileImg'> {
+  eventId?: number
+  title?: string
+  location?: string
+  type?: MeetTypes
+  eventImg?: string
+  eventDeadLine?: string
+  /**
+   * TODO : 마감 여부 boolean 필요
+   */
+}
+
+export interface MeetDetail
+  extends Omit<Meet, 'profileId' | 'profileName' | 'profileImg'> {
+  /**
+   * TODO : 왜 meet랑 meetdetail의 profile 필드가 각각 다를까?
+   */
+  profile?: Pick<Meet, 'profileId' | 'profileName' | 'profileImg'>
+  recruitType?: string
+  description?: string
+  recruitCount?: number
+  /**
+   * TODO : 마감 여부 boolean 필요
+   */
+}
+
+export interface MeetCandicate
+  extends Pick<
+    User,
+    'profileId' | 'profileImg' | 'profileName' | 'openChatUrl' | 'location'
+  > {
+  /**
+   * TODO : descrption이랑 profileDescription이 다른건가? 같은거 같은데 필드가 User와 다르다
+   */
+  profileDescription?: string
+  isMatched?: boolean
+}
+
+export interface MeetAccept {
+  profileIds?: string[]
+}
+
+export interface PerformanceImminent {
+  id?: string
+  title?: string
+  startDate?: string
+  image?: string
+}
+
+export interface Performance {
+  performId?: string
+  performTitle?: string
+  performImg?: string
+  performStartDate?: string
+  performLocation?: string
+  performGenres?: GenreTypes[]
+  profile?: {
+    id?: string
+    imgUrl?: string
+    name?: string
+  }
+}
+
+export interface PerformanceDetail extends Performance {
+  performStartTime?: string
+  performDescription?: string
+  performAddress?: string
+}
+
+export interface CommentCreate {
+  review?: string
+}
+
+export interface Comment {
+  reviewContent?: string
+  reviewId?: string
+  writerName?: string
+  createdDate?: string
+}
+
+// export interface PerformanceSearch {
+// }
