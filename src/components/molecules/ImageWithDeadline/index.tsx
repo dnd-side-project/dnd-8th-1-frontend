0177f2968c9cae2e401650178767a9073901d600
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-15 05:28:40 +0900

    🐛 fix: styleClass 받을 수 있는 형태로 수정

--- src/components/molecules/ImageWithDeadline/index.tsx
+++ src/components/molecules/ImageWithDeadline/index.tsx
@@ -5,15 +5,22 @@ import { calculateDday, isDeadLine } from '@utils'
 interface ImageWithDeadeLineProps {
   deadLine: string
   imgUrl: string
+  styleClass?: string
 }
 
-const ImageWithDeadLine = ({ deadLine, imgUrl }: ImageWithDeadeLineProps) => {
+const ImageWithDeadLine = ({
+  deadLine,
+  imgUrl,
+  styleClass,
+}: ImageWithDeadeLineProps) => {
   const isDead = isDeadLine(deadLine)
   const dday = calculateDday(deadLine)
   const showDday = dday <= 7
 
   return (
-    <div className="relative h-[110px] w-[96px] overflow-hidden rounded-[6px]">
+    <div
+      className={`relative h-[110px] w-[96px] overflow-hidden rounded-[6px] ${styleClass}`}
+    >
       {isDead && (
         <Center className="absolute h-[100%] w-[100%] bg-gray-700 bg-opacity-60">
           <Text className="s z-10 text-caption font-bold text-gray-100">
