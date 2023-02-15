4b1599cd0d972820caa9e6e3d519e9ca78b17cdd
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-16 03:31:17 +0900

    🐛 fix : 헤더가 component에 포함되는 레이아웃 버그 수정

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -4,9 +4,9 @@ import { SidebarMenu } from '@components'
 const Header = () => {
   return (
     <div
-      className={`fixed top-0 z-[999] box-border block flex h-[52px] w-[375px] justify-between bg-gray-900 px-[16px] py-[14px]`}
+      className={`top-0 z-[999] box-border block flex h-[52px] w-[375px] justify-between bg-gray-900 px-[16px] py-[14px]`}
     >
-      <Box className="z-[999] text-gray-200">Logo</Box>
+      <Box className="text-gray-200 z-[999]">Logo</Box>
       <SidebarMenu />
     </div>
   )
