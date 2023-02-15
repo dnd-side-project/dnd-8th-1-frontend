4eff1ba87d2b48f130096012d9699abe08995742
jinyoung <sm099@naver.com>
2023-02-16 02:14:46 +0900

    🐛 fix : 빈 배열일 경우 분기 처리 추가
    분기 처리 추가 완료

--- src/components/organisms/PerformanceList/PerformanceList.stories.tsx
+++ src/components/organisms/PerformanceList/PerformanceList.stories.tsx
@@ -11,51 +11,43 @@ export default {
   parameters: {
     componentSubtitle: 'PerformanceList 컴포넌트',
   },
-  argTypes: {
-    performances: {
-      defaultValue: [
-        {
-          performId: '1',
-          performTitle: '1월 부산에서 공연할 예정인 혁오 밴드 입니다!!',
-          performImg: 'https://picsum.photos/96/110',
-          performStartDate: '2021-01-01',
-          performLocation: '부산',
-          performGenres: ['힙합'],
-          profile: {
-            id: '1',
-            imgUrl: 'https://picsum.photos/96/110',
-            name: '혁오 밴드(오혁, 임동건, 임현제, 이인우)',
-          },
-        },
-        {
-          performId: '2',
-          performTitle: '2월 공연',
-          performImg: 'https://picsum.photos/96/110',
-          performStartDate: '2021-01-01',
-          performLocation: '부산',
-          performGenres: ['힙합'],
-          profile: {
-            id: '1',
-            imgUrl: 'https://picsum.photos/96/110',
-            name: '김철수',
-          },
-        },
-      ],
-    },
-  },
   decorators: [
     (Story) => (
-      <Layout>
-        <div className="mt-[36px] flex w-full flex-col items-center bg-gray-900 py-[18px]">
+      <div className="w-[375px]">
+        <div className="flex w-full flex-col items-center bg-gray-900 py-[18px]">
           <Story />
         </div>
-      </Layout>
+      </div>
     ),
   ],
 } as ComponentMeta<typeof PerformanceList>
 
-const Template: ComponentStory<typeof PerformanceList> = (args) => (
-  <PerformanceList {...args} />
-)
+const DUMMY = {
+  performId: '1',
+  performTitle: '1월 부산에서 공연할 예정인 혁오 밴드 입니다!!',
+  performImg: 'https://picsum.photos/96/110',
+  performStartDate: '2021-01-01',
+  performLocation: '부산',
+  performGenres: ['힙합'],
+  profile: {
+    id: '1',
+    imgUrl: 'https://picsum.photos/96/110',
+    name: '혁오 밴드(오혁, 임동건, 임현제, 이인우)',
+  },
+}
+
+const Template: ComponentStory<typeof PerformanceList> = (args) => {
+  return <PerformanceList {...args} />
+}
 
 export const Default = Template.bind({})
+
+Default.args = {
+  performances: Array(4).fill(DUMMY),
+}
+
+export const EmptyList = Template.bind({})
+
+EmptyList.args = {
+  performances: [],
+}
--- src/components/organisms/PerformanceList/index.tsx
+++ src/components/organisms/PerformanceList/index.tsx
@@ -6,6 +6,19 @@ interface PerformanceListProps {
 }
 
 const PerformanceList = ({ performances }: PerformanceListProps) => {
+  const isEmptyPerformances = performances.length === 0
+  if (isEmptyPerformances) {
+    return (
+      <div className="mt-[77.87px] mb-[118px] flex w-[100%] flex-col items-center justify-center">
+        {/* TODO: 디자인 팀에 이미지 받기
+      레이아웃을 위한 임시 박스 */}
+        <div className="h-[133.45px] w-[162.12px] bg-green-light"></div>
+        <p className=" mt-[18.69px] text-center text-body1 text-gray-500 ">
+          조건에 맞는 결과가 없습니다
+        </p>
+      </div>
+    )
+  }
   return (
     <>
       {performances.map((performance) => (
