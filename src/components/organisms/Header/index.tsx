3667ccdb580f09533687c8a35e95c6850da103ef
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-16 03:33:45 +0900

    🐛 fix: 사이드바 레이아웃 버그로 인한 이전 상태로 변경

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -4,7 +4,7 @@ import { SidebarMenu } from '@components'
 const Header = () => {
   return (
     <div
-      className={`top-0 z-[999] box-border block flex h-[52px] w-[375px] justify-between bg-gray-900 px-[16px] py-[14px]`}
+      className={`fixed top-0 z-[999] box-border block flex h-[52px] w-[375px] justify-between bg-gray-900 px-[16px] py-[14px]`}
     >
       <Box className="text-gray-200 z-[999]">Logo</Box>
       <SidebarMenu />
