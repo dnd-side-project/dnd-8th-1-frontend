75a86fb253713113e66a73c14c494c203ecdd815
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-16 00:56:34 +0900

    ♻️ refactor: 타입 정의 방식 수정

--- src/components/organisms/CollaboList/index.tsx
+++ src/components/organisms/CollaboList/index.tsx
@@ -3,7 +3,7 @@ import { Meet } from '@types'
 import CollaboListItem from './CollaboListItem'
 
 interface CollaboListProps {
-  collaboItems: Array<Meet>
+  collaboItems: Meet[]
 }
 
 const CollaboList = ({ collaboItems }: CollaboListProps) => {
