fe52a5aeb397a96916761ccc3313978d2b310e78
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-16 03:40:40 +0900

    🐛 fix: 페이지 Layout 버그 해결

--- src/components/templates/Layout/index.tsx
+++ src/components/templates/Layout/index.tsx
@@ -1,21 +1,13 @@
-import { Box } from '@chakra-ui/react'
-import { ReactElement, useRef } from 'react'
+import { ReactElement } from 'react'
 import { Header } from '@components'
 
 const Layout = ({ children }: { children: ReactElement }) => {
   return (
-    // TODO: 디자이너에게 패딩 값 전달 받아서 반영
-    <Box
-      maxW={375}
-      w={375}
-      marginLeft="auto"
-      marginRight="auto"
-      overflow="hidden"
-    >
+    <div className="mx-auto h-[100vh] w-[375px] overflow-hidden">
       <Header />
       {children}
       {/* 푸터  */}
-    </Box>
+    </div>
   )
 }
 
