4fd4baf3c546174e751bc5fd34d5ecc498548478
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2023-02-16 23:00:27 +0900

    ✨ feat: 만나기 메인 페이지 마크업 (#98)
    * ✨ feat: 만나기 메인 페이지 마크업
    
    * 🐛 fix: 배포 에러 수정
    
    * 🍱 asset: 게시글 없을 때의 이미지 파일 추가
    
    * 💄 design: 게시글 없을 때, 이미지 추가

--- src/pages/meet/index.tsx
+++ src/pages/meet/index.tsx
@@ -1,5 +1,119 @@
-const MeetPage = () => {
-  return <div>MeetPage</div>
+import Head from 'next/head'
+import { MeetResponse } from '@types'
+import { MeetBanner, FilterButton, Pagination, CollaboList } from '@components'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { MEET_DUMMUMY } from '../../dummy'
+import { Center } from '@chakra-ui/react'
+
+// TODO: api 붙이기
+const MeetPage = ({ data }: MeetResponse) => {
+  const router = useRouter()
+  const { page } = router.query
+
+  const { content, pageNumber, totalPages } =
+    MEET_DUMMUMY[page ? parseInt(page as string) - 1 : 0]
+  const [currentPage, setCurrentPage] = useState(pageNumber)
+
+  const [currentRegion, setCurrentRegion] = useState('')
+  const [currentType, setCurrentType] = useState('')
+
+  const [collaboItems, setCollaboItems] = useState(content)
+
+  // TODO: 임시 페이지네이션 로직
+  useEffect(() => {
+    router.push(`?page=${currentPage + 1}`)
+    setCollaboItems(MEET_DUMMUMY[currentPage].content)
+  }, [currentPage])
+
+  // TODO: 임시 필터 로직
+  useEffect(() => {
+    console.log(currentRegion)
+    if (currentRegion === '') {
+      if (currentType === '') {
+        setCollaboItems([...content])
+      } else {
+        const filtered = content.filter((item) => item.type === currentType)
+        setCollaboItems([...filtered])
+      }
+    } else {
+      if (currentType === '') {
+        const filtered = content.filter(
+          (item) => item.location == currentRegion,
+        )
+        console.log(filtered)
+        setCollaboItems([...filtered])
+      } else {
+        const filtered = content.filter(
+          (item) => item.location == currentRegion && item.type === currentType,
+        )
+        setCollaboItems([...filtered])
+      }
+    }
+  }, [currentRegion])
+
+  useEffect(() => {
+    if (currentType === '') {
+      if (currentRegion === '') {
+        setCollaboItems([...content])
+      } else {
+        const filtered = content.filter(
+          (item) => item.location === currentRegion,
+        )
+        setCollaboItems([...filtered])
+      }
+    } else {
+      if (currentRegion === '') {
+        const filtered = content.filter((item) => item.type == currentType)
+        setCollaboItems([...filtered])
+      } else {
+        const filtered = content.filter(
+          (item) => item.location == currentRegion && item.type === currentType,
+        )
+        setCollaboItems([...filtered])
+      }
+    }
+  }, [currentType])
+
+  return (
+    <>
+      <Head>
+        <title>콜라보 · 쉐어 - Danverse</title>
+      </Head>
+
+      <MeetBanner />
+
+      <main>
+        <div className="mt-[43px] flex items-center justify-between px-[16px]">
+          <div className="flex gap-[8px]">
+            <FilterButton
+              type="region"
+              handleSelected={(filter) => setCurrentRegion(filter)}
+            />
+            <FilterButton
+              type="meet"
+              handleSelected={(filter) => setCurrentType(filter)}
+            />
+          </div>
+          <button onClick={() => router.push('meet/edit')}>작성하기</button>
+        </div>
+
+        <section>
+          <CollaboList collaboItems={collaboItems} />
+        </section>
+
+        <Center className="mb-[30px]">
+          <Pagination
+            currentPage={pageNumber + 1}
+            totalPages={totalPages}
+            handleChangePage={(page) => {
+              setCurrentPage(page - 1)
+            }}
+          />
+        </Center>
+      </main>
+    </>
+  )
 }
 
 export default MeetPage
