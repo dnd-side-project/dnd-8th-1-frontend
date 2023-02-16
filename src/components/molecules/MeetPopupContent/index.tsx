3e63ab91e01ba7184fdffb2513bf6c23a0b50ec4
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2023-02-16 16:57:39 +0900

    ✨ feat: FilterButton 컴포넌트 구현 (#90)

---
+++ src/components/molecules/MeetPopupContent/index.tsx
@@ -0,0 +1,23 @@
+import { MEET_TYPE } from '@constants'
+
+interface MeetPopupContentProps {
+  handleOnClick: (meetType: string) => void
+}
+
+const MeetPopupContent = ({ handleOnClick }: MeetPopupContentProps) => {
+  return (
+    <ul className=" flex h-[94px] w-[90px] flex-col gap-[9px] rounded-[8px] border-[1px] border-[gray-600] border-[solid] bg-gray-700 px-[6px] py-[6px]">
+      {MEET_TYPE.map((type) => (
+        <li
+          key={type}
+          className="flex h-[36px] w-[78px] cursor-pointer items-center justify-center rounded-[4px] text-center text-body2 text-gray-400 hover:bg-gray-600 hover:font-bold hover:text-green-light"
+          onClick={() => handleOnClick(type)}
+        >
+          <span>{type}</span>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default MeetPopupContent
