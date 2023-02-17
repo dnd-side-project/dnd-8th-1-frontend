47c06de927f9edc243387d36fdc23eaa44dceff9
jinyoung <87177577+jinyoung234@users.noreply.github.com>
2023-02-18 02:27:10 +0900

    ✨ feat : MeetDetailCard 컴포넌트 구현 (#120)
    * ✨ feat : MeetDetailCard 컴포넌트 구현
    
    * 💡 comment : 인터페이스에 주석 추가

---
+++ src/components/molecules/MeetDetailCard/index.tsx
@@ -0,0 +1,48 @@
+interface MeetDetailCardProps {
+  /**
+   * TODO : 추후 '개인' | '팀'에 대한 타입 교체 필요
+   */
+  recruitType: '개인' | '팀'
+  recruitCount: number
+  deadline: string
+  location: string
+}
+const DetailCard = ({
+  recruitType,
+  recruitCount,
+  deadline,
+  location,
+}: MeetDetailCardProps) => {
+  const deadlineDate = `${deadline.slice(0, 4)}.${deadline.slice(
+    5,
+    7,
+  )}.${deadline.slice(8, 10)}`
+  return (
+    <div className="flex h-[98px] w-[343px] flex-col items-start justify-center rounded-[10px] border border-gray-700 bg-gray-900 px-[18px] py-[18px]">
+      <div>
+        <span className="mr-[12px] text-body2 font-normal text-gray-400">
+          모집 인원
+        </span>
+        <span className="text-body2 font-normal text-gray-100">
+          {recruitType} {recruitCount}명
+        </span>
+      </div>
+      <div>
+        <span className="mr-[12px] text-body2 font-normal text-gray-400">
+          모집 기간
+        </span>
+        <span className="text-body2 font-normal text-gray-100">
+          ~ {deadlineDate}
+        </span>
+      </div>
+      <div>
+        <span className="mr-[38px] text-body2 font-normal text-gray-400">
+          지역
+        </span>
+        <span className="text-body2 font-normal text-gray-100">{location}</span>
+      </div>
+    </div>
+  )
+}
+
+export default DetailCard
