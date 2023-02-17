895622f256389b6e79c2de89916ea0f9f21ba8dd
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-18 07:36:31 +0900

    ✨ feat: CountPeople 컴포넌트 구현

---
+++ src/components/molecules/CountPeople/CountPeople.stories.tsx
@@ -0,0 +1,35 @@
+import CountPeople from '.'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Center } from '@chakra-ui/react'
+
+export default {
+  component: CountPeople,
+  title: 'Molecules/CountPeople',
+  argTypes: {
+    profileImage: { defaultValue: 'https://picsum.photos/200' },
+    currentType: {
+      defaultValue: '댄서',
+      control: 'inline-radio',
+      options: ['댄서', '댄스팀'],
+    },
+    handleCountPeople: {
+      defaultValue: (count: number) => console.log('현재 인원 수', count),
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Center className="h-[200px] w-[375px] bg-[#000]">
+        <Story />
+      </Center>
+    ),
+  ],
+  parameters: {
+    componentSubtitle: '아바타 컴포넌트 (유저의 프로필 사진 표시)',
+  },
+} as ComponentMeta<typeof CountPeople>
+
+const Template: ComponentStory<typeof CountPeople> = (args) => (
+  <CountPeople {...args} />
+)
+
+export const Default = Template.bind({})
