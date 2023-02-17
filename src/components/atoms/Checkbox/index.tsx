44340a3a2a80feead5519a37ad97a2fefad9cc58
jinyoung <sm099@naver.com>
2023-02-17 22:36:42 +0900

    💄 design : checkbox custom style로 변경
    custom style로 변경 완료

--- src/components/atoms/Checkbox/index.tsx
+++ src/components/atoms/Checkbox/index.tsx
@@ -1,5 +1,4 @@
 import { ChangeEvent, forwardRef, InputHTMLAttributes } from 'react'
-
 interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
   checked?: boolean
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void
@@ -7,7 +6,19 @@ interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
   ({ ...rest }: CheckboxProps, ref) => {
-    return <input type="checkbox" ref={ref} className="h-3 w-3" {...rest} />
+    return (
+      <>
+        <label className="flex select-none items-center" htmlFor="checkbox">
+          <input
+            id="checkbox"
+            type="checkbox"
+            ref={ref}
+            className={`bg-checked h-[18px] w-[18px] cursor-pointer appearance-none rounded-[50%] bg-gray-100 checked:bg-green-light`}
+            {...rest}
+          />
+        </label>
+      </>
+    )
   },
 )
 
