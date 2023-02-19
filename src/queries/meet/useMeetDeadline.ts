a0aac6b348175c40f72eeec5e0cbe8ac3014e0b4
jinyoung <sm099@naver.com>
2023-02-19 15:29:24 +0900

    ✨ feat : 만나기 조기 마감 관련 커스텀 훅 추가
    커스텀 훅 추가 완료

---
+++ src/queries/meet/useMeetDeadline.ts
@@ -0,0 +1,26 @@
+import { eventAPI } from '@apis'
+import { QUERY_KEY } from '@constants'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { MeetCloseRequest } from '@types'
+import { useRouter } from 'next/router'
+
+const patchEarlyDeadline = async (payload: MeetCloseRequest) => {
+  const { data } = await eventAPI.close(payload)
+  return data
+}
+
+const useMeetDeadline = (meetId: number) => {
+  const router = useRouter()
+  const queryClient = useQueryClient()
+  return useMutation(
+    (payload: MeetCloseRequest) => patchEarlyDeadline(payload),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([QUERY_KEY.MEET_DETAIL, meetId])
+        router.push(`/meet/${meetId}`)
+      },
+    },
+  )
+}
+
+export default useMeetDeadline
