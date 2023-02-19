c2112c67a81f40149e3a8874bfb68deb99928431
jinyoung <sm099@naver.com>
2023-02-19 17:21:06 +0900

    🐛 fix : 신청 완료 변경 상태 신청 관련 커스텀 훅에 추가
    신청 완료 성공 시 상태를 변경하기 위한 수정

--- src/queries/meet/useRequestCandidate.ts
+++ src/queries/meet/useRequestCandidate.ts
@@ -2,19 +2,24 @@ import { eventAPI } from '@apis'
 import { QUERY_KEY } from '@constants'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { MeetApplyRequest } from '@types'
+import { Dispatch, SetStateAction } from 'react'
 
 const postRequestCandidate = async (payload: MeetApplyRequest) => {
   const { data } = await eventAPI.apply(payload)
   return data
 }
 
-const useRequestCandidate = (eventId: number) => {
+const useRequestCandidate = (
+  eventId: number,
+  setIsCompleted: Dispatch<SetStateAction<boolean>>,
+) => {
   const queryClient = useQueryClient()
   return useMutation(
     (payload: MeetApplyRequest) => postRequestCandidate(payload),
     {
       onSuccess: () => {
         queryClient.invalidateQueries([QUERY_KEY.MEET_CANDIDATE_VIEW, eventId])
+        setIsCompleted(true)
       },
     },
   )
