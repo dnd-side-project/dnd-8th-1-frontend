d8edb4c5139a959f012d74850ba5d5b3afa7b7cb
jinyoung <sm099@naver.com>
2023-02-19 23:34:30 +0900

    🚑 hotfix : Mixcontent cors 에러 해결을 위한 meta 태그 추가
    Mixed Content: The page at 'https://...' was loaded over HTTPS, but requested an insecure resource' 해결을 위한 메타태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
