7682ae8d852551b57507ffde717211a3f987933f
jinyoung <sm099@naver.com>
2023-02-19 14:56:38 +0900

    ✨ feat : 신청자 수락 관련 커스텀 훅 추가
    커스텀 훅 추가 완료

---
+++ src/queries/meet/useAcceptCandidate.ts
@@ -0,0 +1,27 @@
+import { eventAPI } from '@apis'
+import { QUERY_KEY } from '@constants'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { MeetAcceptRequest } from '@types'
+
+const patchAcceptCandidate = async (
+  eventId: number,
+  payload: MeetAcceptRequest,
+) => {
+  const { data } = await eventAPI.accept(eventId, payload)
+  return data
+}
+
+const useAcceptCandidate = () => {
+  const queryClient = useQueryClient()
+  return useMutation(
+    (payload: MeetAcceptRequest & { eventId: number }) =>
+      patchAcceptCandidate(payload.eventId, payload),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([QUERY_KEY.MEET_CANDIDATE_VIEW])
+      },
+    },
+  )
+}
+
+export default useAcceptCandidate
