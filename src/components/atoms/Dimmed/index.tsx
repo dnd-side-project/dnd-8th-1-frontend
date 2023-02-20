cb897dd8cceb57354a66186def2b40f5456b9589
jinyoung <sm099@naver.com>
2023-02-21 00:52:27 +0900

    💄 design : Dimmed 컴포넌트 스타일 수정
    배경 색, opacity 값 추가 및 z-index 조정

--- src/components/atoms/Dimmed/index.tsx
+++ src/components/atoms/Dimmed/index.tsx
@@ -7,7 +7,7 @@ const Dimmed = ({ handleOnClick, styleClass }: DimmedProps) => {
   return (
     <div
       onClick={handleOnClick}
-      className={`fixed z-[999] mx-[auto] w-[375px] ${styleClass}`}
+      className={`fixed z-[998] mx-[auto] w-[375px] bg-[#000000] bg-opacity-60 ${styleClass}`}
     />
   )
 }
