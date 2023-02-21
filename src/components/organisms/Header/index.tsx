047ce7db54640483dc3311675961712331191c11
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-22 00:45:12 +0900

    ✨ feat: Header 에 로고 추가

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,14 +1,19 @@
-import { SidebarMenu } from '@components'
+import { SidebarMenu, StyledImage } from '@components'
 import { usePathname } from 'next/navigation'
+import Logo from '/public/assets/logo/logo_small.png'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Header = () => {
   const pathname = usePathname()
 
   return (
     <div
-      className={`relative top-0 z-[999] box-border block flex h-[52px] w-[375px]  bg-gray-900 px-[16px] py-[14px]`}
+      className={`relative top-0 z-[999] box-border block flex flex h-[52px]  w-[375px] items-center bg-gray-900 py-[14px] px-[18.77px]`}
     >
-      <div className="z-[999] ml-[18.77px] font-bold text-green">Danverse</div>
+      <Link href={'/'}>
+        <Image src={Logo} width={23.89} height={23.89} alt="사이트 로고" />
+      </Link>
       {pathname === '/meet' && (
         <h1 className="ml-[9.93px] cursor-default text-subtitle font-bold text-gray-100">
           콜라보 · 쉐어
