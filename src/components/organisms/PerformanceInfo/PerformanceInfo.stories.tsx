76ca3090a305f445c02ec6a2a1858f41a4a24063
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-22 07:56:38 +0900

    💡 comment: 필요없는 주석 삭제

--- src/components/organisms/PerformanceInfo/PerformanceInfo.stories.tsx
+++ src/components/organisms/PerformanceInfo/PerformanceInfo.stories.tsx
@@ -2,9 +2,6 @@ import PerformanceInfo from '.'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { Center } from '@chakra-ui/react'
 
-/**
- * 스토리북 확인을 위한 샘플 파일입니다.
- */
 export default {
   component: PerformanceInfo,
   title: 'Organisms/PerformanceInfo',
@@ -12,9 +9,6 @@ export default {
     componentSubtitle:
       '공연 상세 페이지에서 공연 시간과 장소를 알려주는 컴포넌트',
   },
-  /**
-   * 컴포넌트에 대해 Container가 필요할 경우 decorators에 추가하면 됩니다.
-   */
   decorators: [
     (Story) => (
       <div className="h-[800px] w-[375px] bg-gray-900">
