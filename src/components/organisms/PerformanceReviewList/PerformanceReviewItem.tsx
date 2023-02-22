475b4f3625c1f94f227907423c08ff7564213c2a
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-22 16:07:59 +0900

    ✨ feat: PerformanceReviewItem 에 리뷰 삭제 로직 연결

--- src/components/organisms/PerformanceReviewList/PerformanceReviewItem.tsx
+++ src/components/organisms/PerformanceReviewList/PerformanceReviewItem.tsx
@@ -1,5 +1,14 @@
+import { useDisclosure } from '@hooks'
 import { Comment } from '@types'
 import dayjs from 'dayjs'
+import dynamic from 'next/dynamic'
+
+const CancelSubmitModal = dynamic(
+  () => import('../../../components/templates/CancelSubmitModal'),
+  {
+    ssr: false,
+  },
+)
 
 interface PerformanceReviewItemProp {
   reviewItem: Comment
@@ -11,28 +20,48 @@ const PerformanceReviewItem = ({
   handleOnDelete,
 }: PerformanceReviewItemProp) => {
   const { content, reviewId, createdDate, profile } = reviewItem
+  const [
+    showReviewDeleteModal,
+    setShowReviewDeleteModal,
+    handleReviewDeleteModalToggle,
+  ] = useDisclosure()
 
   return (
-    <li>
-      <div className="mb-[42px] px-[16px]">
-        <div className="relative mb-[9px] flex gap-[8px]">
-          <span className="text-body2 font-bold text-gray-300">
-            {profile.name}
-          </span>
-          <span className="text-body2 text-gray-500 ">
-            {dayjs(createdDate).format('· M월 D일')}
-          </span>
-          {/* TODO: 현재 로그인한 유저의 후기일 경우에만 렌더링 */}
-          <button
-            className="absolute right-0 text-body2 text-gray-400"
-            onClick={() => handleOnDelete(reviewId)}
-          >
-            삭제
-          </button>
+    <>
+      <li>
+        <div className="mb-[42px] px-[16px]">
+          <div className="relative mb-[9px] flex gap-[8px]">
+            <span className="text-body2 font-bold text-gray-300">
+              {profile.name}
+            </span>
+            <span className="text-body2 text-gray-500 ">
+              {dayjs(createdDate).format('· M월 D일')}
+            </span>
+            {/* TODO: 현재 로그인한 유저의 후기일 경우에만 렌더링 */}
+            <button
+              className="absolute right-0 text-body2 text-gray-400"
+              onClick={() => setShowReviewDeleteModal(true)}
+            >
+              삭제
+            </button>
+          </div>
+          <p className="text-body2 text-gray-100">{content}</p>
         </div>
-        <p className="text-body2 text-gray-100">{content}</p>
-      </div>
-    </li>
+      </li>
+
+      {showReviewDeleteModal && (
+        <CancelSubmitModal
+          showModal={showReviewDeleteModal}
+          setShowModal={setShowReviewDeleteModal}
+          modalContent="후기를 삭제할까요?"
+          submitMessage="네, 삭제할게요"
+          handleOnSubmit={() => {
+            handleOnDelete(reviewId)
+            setShowReviewDeleteModal(false)
+          }}
+        />
+      )}
+    </>
   )
 }
 
