40e2c1f527fa2d785d805972ff6d4f1607f6be39
jinyoung <sm099@naver.com>
2023-02-22 17:00:15 +0900

    🐛 fix : 공연 검색 페이지에서 Header 컴포넌트 안보이게 수정
    수정 완료

--- src/components/templates/Layout/index.tsx
+++ src/components/templates/Layout/index.tsx
@@ -4,13 +4,13 @@ import { usePathname } from 'next/navigation'
 const Layout = ({ children }: { children: ReactElement }) => {
   const pathname = usePathname()
   const isFormHeader = pathname?.includes('/edit')
-
+  const isSearch = pathname?.includes('/search')
   return (
     <div
       id="layout"
       className="relative mx-auto min-h-[100vh] w-[375px] overflow-hidden bg-gray-900 text-gray-100"
     >
-      {isFormHeader ? <FormHeader /> : <Header />}
+      {isFormHeader ? <FormHeader /> : isSearch ? '' : <Header />}
       {children}
       {/* 푸터  */}
     </div>
