a45d6417ef76c663267890fc7dd07f9d433b76a8
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-22 15:20:00 +0900

    💄 design: 컴포넌트의 간격이 안맞는 문제 수정

--- src/components/molecules/ProfileLinkButton/index.tsx
+++ src/components/molecules/ProfileLinkButton/index.tsx
@@ -19,7 +19,7 @@ const ProfileLinkButton = ({
       className="relative h-[76px] w-[343px] rounded-[12px] bg-gray-700 p-[14px]"
       onClick={() => router.push(`/profile/${profileId}`)}
     >
-      <div className="flex h-[100%] w-[100%] justify-start gap-[13px]">
+      <div className="flex h-[100%] w-[100%] items-start justify-start gap-[13px]">
         <Avatar profileImage={profileImage} size={48} />
         <div className="flex flex-col gap-[5px]">
           <span className="text-caption text-gray-300 ">TEAM</span>
