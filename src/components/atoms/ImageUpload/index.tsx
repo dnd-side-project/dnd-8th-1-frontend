520106cacdf088571ebdc7007b35744d1c35bddf
jinyoung <sm099@naver.com>
2023-02-22 23:34:45 +0900

    🐛 fix : ImageUpload 컴포넌트 공연 정보 페이지에서도 사용할 수 있게 변경
    변경 완료

--- src/components/atoms/ImageUpload/index.tsx
+++ src/components/atoms/ImageUpload/index.tsx
@@ -3,25 +3,38 @@ import { useState } from 'react'
 import { Icon } from '@components'
 
 interface ImageUploadProps {
+  isPerformance?: boolean
   initialImage?: string
   handleSetImage?: (img: File) => void
 }
 
-const ImageUpload = ({ handleSetImage, initialImage }: ImageUploadProps) => {
+const ImageUpload = ({
+  isPerformance,
+  handleSetImage,
+  initialImage,
+}: ImageUploadProps) => {
   const [selectedImage, setSelectedImage] = useState(null)
   const [selectedImageURL, setSelectedImageURL] = useState(
     initialImage ? initialImage : '',
   )
 
   return (
-    <div className="h-[173px] w-[343px] overflow-hidden rounded-[8px] border-[1px] border-gray-600">
+    <div
+      className={`h-[173px] w-[343px] overflow-hidden rounded-[8px] border-[1px] border-gray-600 ${
+        isPerformance && 'w-[155px]'
+      }`}
+    >
       <div
-        className={`h-[100%] w-[100%] ${
-          !selectedImage && 'bg-gray-600'
-        } relative`}
+        className={`relative h-[100%] w-[100%] ${
+          !selectedImage && 'bg-gray-700'
+        } `}
       >
         <label htmlFor="input-file" className="h-[100%] w-[100%]">
-          <div className="relative z-[10] flex h-[100%] w-[100%] cursor-pointer items-center justify-center gap-[11px] text-body1 font-bold text-gray-100">
+          <div
+            className={`relative z-[10] flex h-[100%] w-[100%] cursor-pointer items-center justify-center gap-[11px] text-body1 font-bold text-gray-100 ${
+              isPerformance && 'flex-col'
+            }`}
+          >
             <Icon icon="image" size={18} />
             <span>{selectedImage ? '이미지 변경' : '이미지 업로드'}</span>
           </div>
