28d2ff257f2db39d639d59f659f6f876fb29650f
jinyoung <sm099@naver.com>
2023-02-23 17:31:10 +0900

    ✨ feat : 공연 정보 전체 조회 api 추가
    api 모듈 내 공연 정보 전체 조회 api 추가 완료

---
+++ src/apis/domains/performance/index.ts
@@ -0,0 +1,22 @@
+import { unAuthInstance } from '@apis'
+import { CURRENT_YEAR, CURRENT_MONTH, CURRENT_DAY } from '@constants'
+export const performanceAPI = {
+  // 공연 정보 검색 조회
+  search: (teamName: string) => {
+    return unAuthInstance.get(`/api/v1/performances/team?name=${teamName}`)
+  },
+  // 공연 정보 전체 조회(페이지네이션)
+  getAll: (
+    year = CURRENT_YEAR,
+    month = CURRENT_MONTH,
+    day: number | '' = CURRENT_DAY,
+    location = '',
+    genre = '',
+    pageNumber = 0,
+    pageSize = 15,
+  ) => {
+    return unAuthInstance.get(
+      `/api/v1/performances?year=${year}&month=${month}&day=${day}&location=${location}&genre=${genre}&page=${pageNumber}&size=${pageSize}`,
+    )
+  },
+}
