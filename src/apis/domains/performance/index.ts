e35c677508a56c12edad25321d1ec6e454bc0dfb
jinyoung <sm099@naver.com>
2023-02-25 01:38:45 +0900

    ✨ feat : api 모듈 내 공연 정보 등록 api 추가
    api 추가 완료

--- src/apis/domains/performance/index.ts
+++ src/apis/domains/performance/index.ts
@@ -1,8 +1,13 @@
-import { unAuthInstance } from 'apis/utils'
+import { PerformanceEditRequest } from '@types'
+import { authInstance, unAuthInstance } from 'apis/utils'
 
 export const performanceAPI = {
   // 검색 조회
   search: (teamName: string) => {
     return unAuthInstance.get(`/api/v1/performances/team?name=${teamName}`)
+  },
+  // 공연 등록
+  create: (payload: PerformanceEditRequest) => {
+    return authInstance.post('/api/v1/performances', payload)
   },
 }
