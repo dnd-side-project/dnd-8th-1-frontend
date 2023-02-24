29545912ddd3c69ab2cf9498aa0d6095f4c0aba9
jinyoung <sm099@naver.com>
2023-02-25 01:40:58 +0900

    🐛 fix : startTime 부분 dateformat 수정
    날짜 형식 수정 완료

--- src/components/organisms/PerformanceCreateForm/index.tsx
+++ src/components/organisms/PerformanceCreateForm/index.tsx
@@ -10,7 +10,6 @@ import {
   GenreSelect,
 } from '@components'
 import { useForm, useWatch } from 'react-hook-form'
-import { theme } from '@constants'
 import { useRef, useState } from 'react'
 import { useMutation } from '@tanstack/react-query'
 import { uploadImageUrl } from '@queries'
@@ -28,8 +27,7 @@ const PerformanceCreateForm = ({
   handleOnSubmit,
 }: PerformanceCreateFormProps) => {
   const [image, setImage] = useState<File>()
-
-  const { register, control, handleSubmit, setValue, watch } =
+  const { register, control, handleSubmit, setValue } =
     useForm<PerformanceEditRequest>({
       mode: 'onSubmit',
       defaultValues: {
@@ -156,7 +154,10 @@ const PerformanceCreateForm = ({
                 'startDate',
                 date ? dayjs(date).format().slice(0, 10) : '',
               )
-              setValue('startTime', date ? dayjs(date).format() : '')
+              setValue(
+                'startTime',
+                date ? dayjs(date).format().slice(0, 19) : '',
+              )
             }}
           />
         </div>
@@ -190,7 +191,7 @@ const PerformanceCreateForm = ({
               setImage(image)
               const formData = new FormData()
               formData.append('img', image)
-              requestImgUrl(formData)
+              requestImgUrl(formData as FormData)
             }}
           />
         </div>
