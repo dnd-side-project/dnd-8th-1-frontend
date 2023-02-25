399c2b92d5d669837b13af95b184326736e7d7d5
jinyoung <sm099@naver.com>
2023-02-26 03:20:18 +0900

    🐛 fix : 배포 에러 해결
    배포 에러 해결 완료

--- src/types/apis/profile/index.ts
+++ src/types/apis/profile/index.ts
@@ -1,9 +1,8 @@
-import { BaseResponse, MyEvent } from '@types'
+import { BaseResponse, MyEvent, Profile } from '@types'
 
 interface MyEventResponse extends BaseResponse {
   data: MyEvent[]
-import { Profile, BaseResponse } from '@types'
-
+}
 // 임박한 공연 조회 응답
 export interface ProfileRandomResponse extends BaseResponse {
   data: Pick<Profile, 'id' | 'imgUrl' | 'name' | 'type'>[]
