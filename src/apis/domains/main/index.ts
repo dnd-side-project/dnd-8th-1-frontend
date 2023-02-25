8f5bdca20eaf0dfb05098e86adde32dfdce25877
jinyoung <sm099@naver.com>
2023-02-26 02:28:06 +0900

    ✨ feat : 프로필 조회 api 모듈 내에 추가
    추가 완료

--- src/apis/domains/main/index.ts
+++ src/apis/domains/main/index.ts
@@ -1,6 +1,11 @@
 import { unAuthInstance } from '@apis'
 export const mainAPI = {
+  // 후기 6개 조회
   getComment: () => {
     return unAuthInstance.get(`/api/v1/performances/recent/reviews`)
+  },
+  // 프로필 6개 조회
+  getProfile: () => {
+    return unAuthInstance.get(`/api/v1/profiles/home`)
   },
 }
