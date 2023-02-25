27cb411e37296fcc28ef1f6eb64eacbe9b2e18be
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-26 06:26:59 +0900

    ✨ feat: ProfileDescription 컴포넌트 구현

---
+++ src/components/molecules/ProfileDescription/index.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const ProfileDescription = ({ description }: { description: string }) => {
+  const [showMore, setShowMore] = useState(false)
+  const showMoreButtonStyle =
+    'h-[46px] w-[343px] bg-gray-700 text-body2 font-bold text-gray-100 rounded-b-[10px] border-t-[1px] border-t-gray-600'
+
+  return (
+    <div className="w-[343px] rounded-[10px] bg-gray-700">
+      <div
+        className={`${
+          !showMore && 'h-[143px] overflow-hidden '
+        } min-h-[143px] whitespace-pre-wrap p-[16px] text-body2 text-gray-100`}
+      >
+        <p
+          className={`${!showMore && 'h-[105px] overflow-hidden line-clamp-5'}`}
+        >
+          {description}
+        </p>
+      </div>
+
+      {showMore ? (
+        <button
+          className={showMoreButtonStyle}
+          onClick={() => setShowMore(false)}
+        >
+          접기
+        </button>
+      ) : (
+        <button
+          onClick={() => setShowMore(true)}
+          className={showMoreButtonStyle}
+        >
+          더보기
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default ProfileDescription
