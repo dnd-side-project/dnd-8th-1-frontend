3bde75b7e515acff022a0babb6ac2ff83c8b1790
jinyoung <sm099@naver.com>
2023-02-25 17:50:44 +0900

    ✨ feat : MainMeetList 컴포넌트 구현
    MainMeetSection 내 존재하는 List 컴포넌트 구현

---
+++ src/components/organisms/MainMeetSection/MainMeetList.tsx
@@ -0,0 +1,18 @@
+import { Meet } from '@types'
+import MainMeetListItem from './MainMeetListItem'
+
+interface MainMeetSectionProps {
+  meetLists: Omit<Meet, 'deadline'>[]
+}
+
+const MainMeetList = ({ meetLists }: MainMeetSectionProps) => {
+  return (
+    <section className="no-scrollbar mt-[28px] flex overflow-x-scroll">
+      {meetLists.map((meetItem) => (
+        <MainMeetListItem key={meetItem.id} meetItem={meetItem} />
+      ))}
+    </section>
+  )
+}
+
+export default MainMeetList
