ea10ab48fda7359d801326cf21a259ab9c054707
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-27 01:03:13 +0900

    ✨ feat: PortfolioButton에 모달 여는 로직 추가

--- src/components/organisms/ProfileButtonGroup/PortfolioButton.tsx
+++ src/components/organisms/ProfileButtonGroup/PortfolioButton.tsx
@@ -1,15 +1,34 @@
 import { Icon } from '@components'
 import { theme, ProfileButtonStyle } from '@constants'
+import { useDisclosure } from '@hooks'
+import { Portfolio } from '@types'
 
-const PortfolioButton = () => {
+import dynamic from 'next/dynamic'
+
+const PortfolioLinkModal = dynamic(() => import('../PortfolioLinkModal'), {
+  ssr: false,
+})
+
+const PortfolioButton = ({ portfolio }: { portfolio: Portfolio }) => {
+  const [showModal, setShowModal, handleToggle] = useDisclosure()
+
   return (
-    <button
-      className={ProfileButtonStyle}
-      onClick={() => console.log('모달을 띄운다.')}
-    >
-      <Icon icon="user-square-mono" color={theme.colors.gray[900]} size={20} />
-      <span>작업물 보기</span>
-    </button>
+    <>
+      <button className={ProfileButtonStyle} onClick={handleToggle}>
+        <Icon
+          icon="user-square-mono"
+          color={theme.colors.gray[900]}
+          size={20}
+        />
+        <span>작업물 보기</span>
+      </button>
+
+      <PortfolioLinkModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        portfolio={portfolio}
+      />
+    </>
   )
 }
 
