764619efad927a7d24d5df7c6d83e63855e687c5
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-27 02:54:53 +0900

    ♻️ refactor: 코드 리뷰 반영

--- src/components/organisms/MyProfileMenu/MyProfileMenuItem.tsx
+++ src/components/organisms/MyProfileMenu/MyProfileMenuItem.tsx
@@ -13,8 +13,8 @@ const MyProfileMenuItem = ({
   return (
     <li className="relative cursor-pointer py-[20px] px-[18px] text-gray-100">
       <div className="flex flex-col gap-[9px]">
-        <span className="text-body1 font-bold font-bold">{content}</span>
-        <span className="text-body2">{description}</span>
+        <span className="text-body1 font-bold">{content}</span>
+        <span className="text-body2 text-[#CECECE]">{description}</span>
       </div>
       <div className="absolute top-[22px] right-[20.56px]">
         <Icon icon="arrow-right" size={21} color={theme.colors.gray[100]} />
