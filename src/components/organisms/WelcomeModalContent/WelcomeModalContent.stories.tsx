b809ad14f702879254458625d610cac4e9463e30
dar-jeeling <o_o___t@pusan.ac.kr>
2023-02-27 02:43:56 +0900

    ✨ feat: 모달 내부 컨텐츠 마크업

---
+++ src/components/organisms/WelcomeModalContent/WelcomeModalContent.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import WelcomeModalContent from '.'
+
+export default {
+  component: WelcomeModalContent,
+  title: 'Organisms/WelcomeModalContent',
+  parameters: {
+    componentSubtitle: '가입 시 뜨는 모달 안의 콘텐츠',
+  },
+
+  decorators: [
+    (Story) => (
+      <div className="w-[375px]">
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof WelcomeModalContent>
+
+const Template: ComponentStory<typeof WelcomeModalContent> = () => (
+  <WelcomeModalContent />
+)
+
+export const Default = Template.bind({})
