c82edd66fd36a5478bb5e0a42f5a89c783fbd61c
jinyoung <sm099@naver.com>
2023-02-28 18:38:16 +0900

    ♻️ refactor : meet 쿼리 키에 영향 받는 hook들 추가적인 수정 진행
    수정 완료

--- src/queries/meet/hooks/useCandidate.ts
+++ src/queries/meet/hooks/useCandidate.ts
@@ -4,16 +4,14 @@ import { MeetApplicantsResponse } from '@types'
 import { useQuery } from '@tanstack/react-query'
 import { meetKeys } from '@queries'
 
-const getCandidate = async (eventId: number) => {
+export const getCandidate = async (eventId: number) => {
   const { data }: AxiosResponse<MeetApplicantsResponse> =
     await eventAPI.getApplicants(eventId)
   return data
 }
 
-const useCandidate = (eventId: number) => {
+export const useCandidate = (eventId: number) => {
   return useQuery([meetKeys.candidate], () => getCandidate(eventId), {
     enabled: !!eventId,
   })
 }
-
-export default useCandidate
