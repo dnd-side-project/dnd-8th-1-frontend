c60a2be5a616c9d770d5baad7944175ac67405df
dar-jeeling <o_o___t@pusan.ac.kr>
2023-03-03 04:29:41 +0900

    ✨ feat: PageLoading 컴포넌트 구현

---
+++ src/components/atoms/PageLoading/index.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import pageLoadingAnimation from './pageLoading.json'
+import Lottie from 'lottie-react'
+
+const PageLoading = () => {
+  const router = useRouter()
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const handleStart = (url: string) =>
+      url !== router.asPath && setLoading(true)
+    const handleComplete = (url: string) =>
+      url === router.asPath && setLoading(false)
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  })
+
+  return loading ? (
+    <div className="fixed top-0 left-0 right-0 bottom-0 z-[999] flex h-[100%] w-[100%] items-center justify-center bg-[#000] opacity-[60%]">
+      <Lottie animationData={pageLoadingAnimation} loop={true} />
+    </div>
+  ) : (
+    <></>
+  )
+}
+
+export default PageLoading
