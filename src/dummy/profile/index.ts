777b7599ad7c25ddccf54560b8f11b5162ae10d0
jinyoung <sm099@naver.com>
2023-03-03 01:56:45 +0900

    🚚 rename : 더미 데이터 dummy 폴더로 이동
    dummy 폴더로 이동 완료

--- src/dummy/profile/index.ts
+++ src/dummy/profile/index.ts
@@ -1,4 +1,46 @@
-import { AppliedEvent, MyEvent, MyPerformance, MyReview } from '@types'
+import {
+  AppliedEvent,
+  GenreTypes,
+  MyEvent,
+  MyPerformance,
+  MyReview,
+  RecruitmentType,
+  RegionTypes,
+} from '@types'
+
+// 프로필 조회
+const descriptionLong =
+  '로렘 입숨은 출판이나 그래픽 디자인 \n\n분야에서 폰트, 타이포그래피, 레이아웃 같은 그래픽 요소나시각적 연출을 보여줄 때 사용하는 표준 채우기텍스트로, 최종 결과를 더.로렘 입숨은 출판이나 그래픽 디자인 \n\n분야에서 폰트, 타이포그래피, 레이아웃 같은 그래픽 요소나시각적 연출을 보여줄 때 사용하는 표준 채우기텍스트로, 최종 결과를 더'
+
+const PROFILE_DUMMY = {
+  id: 11,
+  name: '아이키',
+  imgUrl: 'https://picsum.photos/500',
+  location: '부산' as RegionTypes,
+  genre: ['커버댄스', '스트릿댄스'] as GenreTypes[],
+  startDate: '2011-01-05',
+  description: descriptionLong,
+  openChatUrl: 'https://www.allsilver.dev/',
+  portfolio: {
+    youtubeUrl: 'https://www.allsilver.dev/',
+    instagramUrl: 'https://www.allsilver.dev/',
+    tiktokUrl: 'https://www.allsilver.dev/',
+  },
+  type: '댄스팀' as RecruitmentType,
+}
+
+// TODO: 백엔드와 협의 안됨
+const MEMBER_DUMMY = {
+  id: 1,
+  name: '아이키',
+  imgUrl: 'https://picsum.photos/500',
+  email: '123@naver.com',
+  isSignUp: true,
+  role: '권한',
+  profile: PROFILE_DUMMY,
+  // 프로필 없는 경우
+  // profile: null as unknown as Profile,
+}
 
 // 활동 내역
 
