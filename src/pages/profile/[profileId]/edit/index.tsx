17f923ca1e0bd4d401f0ef29f6ab6eb9dad2fe6e
jinyoung <sm099@naver.com>
2023-03-03 04:48:23 +0900

    ✨ feat : 프로필 등록 기능 api 연결
    api 연결 기능 완료

--- src/pages/profile/[profileId]/edit/index.tsx
+++ src/pages/profile/[profileId]/edit/index.tsx
@@ -1,5 +1,30 @@
-const ProfileEditPage = () => {
-  return <div>ProfileEditPage</div>
+import { ProfileCreateForm } from '@components'
+import { useCreateProfile } from '@queries'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+
+interface ProfileEditPageProps {
+  memberId: number
+}
+
+const ProfileEditPage = ({ memberId }: ProfileEditPageProps) => {
+  const { mutate: requestCreateProfile } = useCreateProfile(memberId)
+  return (
+    <ProfileCreateForm
+      handleOnSubmit={(formValues) => {
+        requestCreateProfile(formValues)
+      }}
+    />
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext,
+) => {
+  return {
+    props: {
+      memberId: parseInt(ctx?.params?.profileId as string),
+    },
+  }
 }
 
 export default ProfileEditPage
