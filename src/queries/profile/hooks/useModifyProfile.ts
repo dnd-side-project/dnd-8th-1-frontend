33bad20324af8aa436560fe903736f0523312543
dar-jeeling <o_o___t@pusan.ac.kr>
2023-03-03 23:23:59 +0900

    ✨ feat: 프로필 수정 시, 전역 상태 변경 로직 추가

--- src/queries/profile/hooks/useModifyProfile.ts
+++ src/queries/profile/hooks/useModifyProfile.ts
@@ -3,19 +3,31 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { ProfileEditRequest } from '@types'
 import { useRouter } from 'next/router'
 import { profileKeys } from '@queries'
+import { useRecoilState } from 'recoil'
+import { userAtom } from 'states'
 
 export const patchModifyProfile = async (payload: ProfileEditRequest) => {
-  return profileAPI.modifyProfile(payload)
+  const { data } = await profileAPI.modifyProfile(payload)
+  return data
 }
 
 export const useModifyProfile = (memberId: number) => {
   const router = useRouter()
   const queryClient = useQueryClient()
+  const [user, setUser] = useRecoilState(userAtom)
+
   return useMutation(
     (payload: ProfileEditRequest) => patchModifyProfile(payload),
     {
-      onSuccess: () => {
+      onSuccess: (data) => {
         queryClient.invalidateQueries(profileKeys.detail(memberId))
+
+        setUser({
+          ...user,
+          profile: { ...data.data },
+          hasProfile: true,
+        })
+
         router.push(`/profile/${memberId}`)
       },
     },
