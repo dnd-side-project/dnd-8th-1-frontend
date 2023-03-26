2a3ea795b202c9a21dbe19e4fe841f8771e740bf
jinyoung <sm099@naver.com>
2023-03-26 23:41:35 +0900

    🚚 rename : PortfolioPopup 컴포넌트 domain 폴더 구조로 변경
    변경 완료

--- src/components/domain/profile/edit/index.ts
+++ src/components/domain/profile/edit/index.ts
@@ -1,2 +1,3 @@
 export { default as FormTitleHeader } from '../edit/FormTitleHeader'
 export { default as OpenchatPopup } from './OpenchatPopup'
+export { default as PortfolioPopup } from './PortfolioPopup'
