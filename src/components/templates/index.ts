5568eb0b383529039d36cdddcd3b0eb0daa6eec5
jinyoung <sm099@naver.com>
2023-03-28 00:12:48 +0900

    🔥 remove : StorybookForm 컴포넌트 제거
    더 이상 사용하지 않아 해당 컴포넌트 제거 완료

--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -1,4 +1,3 @@
 export { default as Template } from './Template'
-export { default as StoryBookHOC } from './StorybookForm'
 export { default as Modal } from './Modal'
 export { default as ModalWithSingleButton } from './ModalWithSingleButton'
