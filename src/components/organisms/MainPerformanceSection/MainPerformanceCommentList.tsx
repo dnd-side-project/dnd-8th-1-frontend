a6b105f003f67e8636882490ba4212bf3c89044e
jinyoung <sm099@naver.com>
2023-03-29 17:42:06 +0900

    🐛 fix : MainPerformanceCommentList 컴포넌트 optional chaining 적용
    optional chaining 미적용으로 인해 undefined 발생 하여 에러 발생 한 것 핫 픽스

--- src/components/organisms/MainPerformanceSection/MainPerformanceCommentList.tsx
+++ src/components/organisms/MainPerformanceSection/MainPerformanceCommentList.tsx
@@ -10,7 +10,7 @@ const MainPerformanceCommentList = ({
 }: MainPerformanceCommentListProps) => {
   return (
     <section className="no-scrollbar mt-[21px] flex h-[149px] w-[335px] items-end gap-x-[16px] overflow-hidden overflow-x-scroll">
-      {commentList.map((commentListItem) => (
+      {commentList?.map((commentListItem) => (
         <MainPerformanceCommentListItem
           key={commentListItem.reviewId}
           commentItem={commentListItem}
