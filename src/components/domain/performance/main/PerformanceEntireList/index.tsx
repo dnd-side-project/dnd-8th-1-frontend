5bc24039bfd946c0b40dda883b3a95c86a90a268
jinyoung <sm099@naver.com>
2023-04-29 01:04:44 +0900

    ♻️ refactor : PerformanceEntireList Headless 컴포넌트 적용
    적용 완료

--- src/components/domain/performance/main/PerformanceEntireList/index.tsx
+++ src/components/domain/performance/main/PerformanceEntireList/index.tsx
@@ -1,6 +1,6 @@
 import { PLACEHOLDER_IMG } from '@constants'
-import { Performance } from '@types'
-import dayjs from 'dayjs'
+import { Performance, PerformanceWithDateHeadlessArgs } from '@types'
+import { PerformanceListHeadless } from '@components'
 import Image from 'next/image'
 import PerformanceListItem from '../PerformanceList/PerformanceListItem'
 import emptyImage from '/public/assets/images/graphic_1.png'
@@ -12,73 +12,48 @@ interface PerformanceEntireListProps {
 const PerformanceEntireList = ({
   performances,
 }: PerformanceEntireListProps) => {
-  const makeDate = (date: string) => {
-    const day = dayjs(date)
-    return day.format('M월 D일')
-  }
-
-  const entireDate = Array.from(
-    new Set(performances?.map((item) => item.startDate)),
-  )
-    .sort((a, b) => {
-      return dayjs(b).valueOf() - dayjs(a).valueOf()
-    })
-    .map((item) => makeDate(item))
-
-  const filterPerformance = (date: string) => {
-    return performances.filter((item) => {
-      const performStartDate = makeDate(item.startDate)
-      return date === performStartDate
-    })
-  }
-
-  const entirePerformances = Array.from(
-    { length: entireDate.length },
-    (_, i) => {
-      return {
-        [entireDate[i]]: filterPerformance(entireDate[i]),
-      }
-    },
-  )
-
-  const isEmptyPerformances = performances?.length === 0
   return (
-    <>
-      {isEmptyPerformances ? (
-        <div className="mt-[77.87px] mb-[118px] flex w-[100%] flex-col items-center justify-center">
-          <div className="relative w-[fit] pl-[20px]">
-            <Image
-              src={emptyImage}
-              alt="emptyImage"
-              width={200}
-              height={200}
-              placeholder="empty"
-              blurDataURL={PLACEHOLDER_IMG}
-            />
-            <p className="absolute left-[20px] bottom-0 text-center text-body1 text-gray-500 ">
-              조건에 맞는 결과가 없습니다
-            </p>
+    <PerformanceListHeadless performancesData={performances}>
+      {({
+        filteredPerformanceData,
+        isEmpty,
+      }: PerformanceWithDateHeadlessArgs) => {
+        return isEmpty ? (
+          <div className="mt-[77.87px] mb-[118px] flex w-[100%] flex-col items-center justify-center">
+            <div className="relative w-[fit] pl-[20px]">
+              <Image
+                src={emptyImage}
+                alt="emptyImage"
+                width={200}
+                height={200}
+                placeholder="empty"
+                blurDataURL={PLACEHOLDER_IMG}
+              />
+              <p className="absolute left-[20px] bottom-0 text-center text-body1 text-gray-500 ">
+                조건에 맞는 결과가 없습니다
+              </p>
+            </div>
           </div>
-        </div>
-      ) : (
-        entirePerformances.map((dates, i) => (
-          <section key={i}>
-            <span className="mb-4 block text-body2 text-gray-300">
-              {Object.keys(dates)}
-            </span>
-            {Object.values(dates).map((performances) =>
-              performances.map((performance) => (
-                <PerformanceListItem
-                  key={performance.id}
-                  performance={performance}
-                />
-              )),
-            )}
-            <div className="my-[16px] h-[1px] w-[343px] bg-gray-700" />
-          </section>
-        ))
-      )}
-    </>
+        ) : (
+          filteredPerformanceData.map((dates, i) => (
+            <section key={i}>
+              <span className="mb-4 block text-body2 text-gray-300">
+                {Object.keys(dates)}
+              </span>
+              {Object.values(dates).map((performances) =>
+                performances.map((performance) => (
+                  <PerformanceListItem
+                    key={performance.id}
+                    performance={performance as Performance}
+                  />
+                )),
+              )}
+              <div className="my-[16px] h-[1px] w-[343px] bg-gray-700" />
+            </section>
+          ))
+        )
+      }}
+    </PerformanceListHeadless>
   )
 }
 
